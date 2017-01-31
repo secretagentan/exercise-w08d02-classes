92f78be5c49f3ce4abf7204fe8a4dce0668a7949
an nguyen <hello.an.ng@gmail.com>
2017-01-31 12:44:29 -0800

    calculates rent per unit

--- test.js
+++ test.js
@@ -26,17 +26,36 @@ p3.type = "Timelord";
 console.log(p3.fullname);
 
 let p4 = new Tenant();
-p4.name = "Amy Pond";
-p4.nickname = "'Amelia'";
+p4.name = "Amelia Pond";
+p4.nickname = "'Amy'";
 p4.type = "Human";
 console.log(p4.fullname);
 
-let a1 = new Apartment(10, 1, 1, [p1, p2]);
+let p5 = new Tenant();
+p5.name = "Donna Noble";
+p5.nickname = "'DoctorDonna'";
+p5.type = "TimeHuman";
+console.log(p5.fullname);
+
+let a1 = new Apartment('10a', 1, 1, [p1, p2]);
 // console.log(a1);
 // console.log(a1.tenants);
-console.log(a1.tenants.length);
-console.log(a1.rent);
+console.log(a1.isStudio());
+console.log('Apt. 10a has ' + a1.tenants.length + ' tenant(s)');
+console.log('10a\'s rent is = ' + a1.rent + '/month');
 
-let a2 = new Apartment(11, 2, 1, [p3, p4]);
+let a2 = new Apartment('11a', 2, 1, [p3, p4]);
 // console.log(a2.tenants);
-console.log(a2.tenants.length);
+console.log(a2.isStudio());
+console.log('Apt. 11a has ' + a2.tenants.length + ' tenant(s)');
+console.log('11a\'s rent is = ' + a2.rent + '/month');
+
+let a3 = new Apartment('10b', 1, 1, [p5]);
+// console.log(a3.tenants);
+console.log('Apt. 10b has ' + a3.tenants.length + ' tenant(s)');
+console.log(a3.isStudio());
+console.log('10b\'s rent is = ' + a3.rent + '/month');
+
+
+// a1.moveIn(p5);
+// console.log(a1.tenants);
